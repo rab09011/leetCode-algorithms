c6c4f56f9cb46769855c1d5eb2be7c71c9aecded
Raghdaa Barmo <raghdaa.barmo@gmail.com>
2022-01-23 00:06:13 -0500

    completed twoSum

--- DataStructures/1-twoSum.js
+++ DataStructures/1-twoSum.js
@@ -6,3 +6,24 @@ Input: nums = [2,7,11,15], target = 9
 Output: [0,1]
 Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
 */
+
+const twoSum = function (nums, target) {
+  let map = {};
+  let result = [];
+
+  for (let i = 0; i < nums.length; i++) {
+    let num = nums[i];
+    let needed = target - num;
+
+    if (map.hasOwnProperty(needed)) {
+      let ans = map[needed];
+      result = [i, ans];
+    } else {
+      map[num] = i;
+    }
+  }
+  return result;
+};
+
+let test = [2, 7, 11, 15];
+console.log(twoSum(test, 9));
